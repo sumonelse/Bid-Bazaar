39a9c28a98519b90149e07f20d674293ab118585
sumonelse <sumonelse01@gmail.com>
2025-04-02 20:10:23 +0530

    fix: update import statement for cloudinary configuration

--- server/src/config/cloudinaryConfig.js
+++ server/src/config/cloudinaryConfig.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary"
 import dotenv from "dotenv"
-import { config } from "./config"
+import { config } from "./config.js"
 dotenv.config()
 
 cloudinary.config({
